68795c9659ffd0d4afb651d499c99fcca84a5738
IraDmitrieva <iritrieva@gmail.com>
2023-09-15 20:02:36 +0400

    Update 2023-09-15T16:02:29.809Z

--- src/js/constants/root.js
+++
@@ -1,5 +0,0 @@
-export const ROOT_DIV = document.querySelector('.root');
-export const ROOT_MODAL = document.querySelector('.modal');
-export const ROOT_PRELOADER = document.querySelector('.preloader');
-export const ROOT_COMICSENSE = document.querySelector('.comicsense');
-export const ROOT_INPUT =  document.querySelector('.searchTitle');
